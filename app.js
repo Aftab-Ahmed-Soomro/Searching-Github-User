107e7a55eaca4167e5c71545ba0dc86aae3607a5
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2024-07-25 19:02:46 +0500

    updated files

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 let main = document.getElementById('main');
 let input = document.getElementById('input');
 const searchProfile = () => {
-    // fetch('https://api.github.com/users/$')
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch(`https://api.github.com/users/${input.value}`)
+    // fetch('https://jsonplaceholder.typicode.com/users')
     .then((res)=>{
         return res.json();
     })
     .then((data)=>{
         console.log(data);
-        let filteredData = data.filter((val)=>{
-            return val.username.toLowerCase() == input.value.toLowerCase();
-        })
-        filteredData.forEach((value)=> {
             main.innerHTML = `
             <div class="col-md-3 d-flex justify-content-center align-items-center m-auto mt-4">
                     <div class="card" style="width: 18rem;">
-                    <img src="./frlIf.png" class="card-img-top size" alt="...">
+                    <img src="${data.avatar_url}" class="card-img-top size" alt="...">
                     <div class="card-body d-flex justify-content-center align-items-center m-auto flex-column">
-                        <h6 class="card-title text-center text-body-tertiary mb-4">${value.username}</h6>
-                        <p class="card-text text-center mb-4"><span class="me-4"><i class="fa-solid fa-plus"></i> 0 followers</span><i class="fa-regular fa-user"></i> 0 following</p>
+                        <h6 class="card-title text-center text-body-tertiary mb-4">${data.login}</h6>
+                        <p class="card-text text-center mb-4"><span class="me-4"><i class="fa-solid fa-plus"></i> ${data.followers} followers</span><i class="fa-regular fa-user"></i> ${data.following} following</p>
                         <button type="button" class="btn btn-success text-white">Follow</button>
                     </div>
                     </div>
-            </div>
-            `
-        })
+            </div>`
     })
     .catch((error)=>{
         console.log(error);
